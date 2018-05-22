84efe01c05b67819b73c48fc8490b02b0932a6ca
Gustavo Diel <gustavodiel@hotmail.com>
2018-05-22 08:31:01 -0300

    Initial Commit

---
+++ MLabsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLabsProject
+//
+//  Created by Gustavo Diel on 22/05/18.
+//  Copyright © 2018 Gustavo Diel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
